51ea5ba7074503216a5bc82cc6f3177ebbc4b280
RubenArCol <124272803+RubenArCol@users.noreply.github.com>
2025-02-07 09:28:56 +0100

    feat/empty project

---
+++ T5_P1/src/main/java/com/mycompany/t5_p1/T5_P1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t5_p1;
+
+/**
+ *
+ * @author usuario
+ */
+public class T5_P1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
